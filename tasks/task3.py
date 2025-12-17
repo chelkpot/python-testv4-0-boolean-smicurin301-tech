6291f3df5e761b4b4d0f0419f3cc06b723d46f5f
Мичурин Степан <smicurin301@gmail.com>
2025-12-18 00:39:22 +0500

    Хай4

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-     
+    word1 = input()
+    word2 = input()
+    print(word1 == "awesome" or word2 == "awesome")     
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
